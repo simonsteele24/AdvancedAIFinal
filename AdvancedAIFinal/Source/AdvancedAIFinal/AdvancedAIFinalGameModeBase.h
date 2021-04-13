7367eded0ddaecf128f6d5f4e56d1423705348a8
Simon Steele <simon.steele@mymail.champlain.edu>
2021-04-13 14:53:42 -0400

    Added initial project

---
+++ AdvancedAIFinal/Source/AdvancedAIFinal/AdvancedAIFinalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedAIFinalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDAIFINAL_API AAdvancedAIFinalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
